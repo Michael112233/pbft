1555b68b67236ba221bf3eac76c40b3de85a2f73
Michael112233 <88572680+Michael112233@users.noreply.github.com>
2025-09-21 15:17:43 +0800

    script

--- run_project.sh
+++ run_project.sh
@@ -1,1 +1,6 @@
-/Users/michael/opt/anaconda3/bin/python script/local_experiment.py
+chmod +x environment_setup.sh
+chmod +x local_experiment.py
+chmod +x remote_experiment.py
+
+./script/environment_setup.sh
+python script/local_experiment.py
