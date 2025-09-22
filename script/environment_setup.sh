36be0e4342d7b9341c2eb6e7cf276493c190a530
Michael112233 <88572680+Michael112233@users.noreply.github.com>
2025-09-22 23:34:00 +0800

    add environment setup shell

---
+++ script/environment_setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+pip install requests
+
+wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ~/.profile
